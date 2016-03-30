a2d02eda07c3e8752e3d99a0553d62e3f8f35f9f
Felice De Luca <felicedeluca@me.com>
2016-03-30 12:13:05 +0200

    improvements in Line Sweep Algoritm

--- src/linesweep/LineSweepAlgorithm.java
+++ src/linesweep/LineSweepAlgorithm.java
@@ -1,5 +1,7 @@
 package linesweep;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,12 +12,91 @@ public class LineSweepAlgorithm {
 	
 	Map<Double, Set<Event>> events;
 	
-	public void startOnCircles(Set<Circle> circles){
+	public double startOnCircles(Set<Circle> circles){
 		
+		//Compute Events
 		EventsMng em = new EventsMng();
-		
 		Map<Double, Set<Event>> eventsMap = em.computeStartingAndEndingEvents(circles);
 		
+		//Ordered key list
+		ArrayList<Double> eventsX = new ArrayList<Double>(eventsMap.keySet());
+		Collections.sort(eventsX);
+		
+		double maxPly = 0;
+		
+		for(Double x : eventsX){
+			
+			Set<Event> events = eventsMap.get(x);
+			
+			for (Event e : events){
+				
+				//Setup circles
+				prepareForEvent(e);
+				
+				
+				//Compute all Intersections
+				computeIntersections();
+				
+				//Point of maximum overlap
+				double currPly = pointOfMaximumOverlap();
+
+				
+				//Check Ply
+				if(currPly>maxPly) maxPly = currPly;
+				
+				
+			}
+			
+			
+		}
+		
+		return maxPly;
+		
+	}
+	
+	
+	private void prepareForEvent(Event e){
+	
+		switch(e.type){
+		case OPENING:{
+			if(activeCircles.contains(e.circle))
+				throw new IllegalArgumentException("Trying to activating a circle twice");
+			activeCircles.add(e.circle);
+		}
+			break;
+		case CLOSING:{
+			if(!activeCircles.contains(e.circle))
+				throw new IllegalArgumentException("Trying to close a non active circle");
+			activeCircles.remove(e.circle);
+		}
+			break;
+		case INTERSECTION:{
+			Circle c1 = null;
+			Circle c2 = null;
+			if(!activeCircles.contains(c1) &&
+			   !activeCircles.contains(c2))
+				throw new IllegalArgumentException("Intersection between non active circles");
+
+		}
+			break;
+		default:
+			break;
+		}
+		
+	}
+	
+	private void computeIntersections(){
+
+		
+
+	}
+	
+	private double pointOfMaximumOverlap(){
+		
+		double p = 0;
+		
+		
+		return p;
 	}
 
 }
